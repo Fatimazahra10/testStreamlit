c36c610c7ab930904c9686bb9acf6ae7865a38e7
Fatimazahra10 <101047121+Fatimazahra10@users.noreply.github.com>
2024-12-21 15:37:16 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -7,4 +7,4 @@ st.subheader("test 1")
 st.sidebar.image("https://seeklogo.com/images/E/ecole-hassania-des-travaux-publics-ehtp-logo-3D5770F217-seeklogo.com.png")
 choix=st.sidebar.selectbox('Select app type', ['-- choose application --', 'Image analysis', 'Face analysis', 'OCR', 'Thumbnail Image'])
 if choix== 'Image analysis':
-st.file_uploader('load image', type=['png', 'jpg', 'jpeg'])
+  st.file_uploader('load image', type=['png', 'jpg', 'jpeg'])
