1f24910fc1157b362e82be936b0989323cf54dfa
Fatimazahra10 <101047121+Fatimazahra10@users.noreply.github.com>
2024-12-21 15:36:53 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,4 +5,6 @@ st.title("EMC2 first app")
 st.header("App for testing azure AI services")
 st.subheader("test 1")
 st.sidebar.image("https://seeklogo.com/images/E/ecole-hassania-des-travaux-publics-ehtp-logo-3D5770F217-seeklogo.com.png")
-st.sidebar.selectbox('Select app type', ['-- choose application --', 'Image analysis', 'Face analysis', 'OCR', 'Thumbnail Image'])
+choix=st.sidebar.selectbox('Select app type', ['-- choose application --', 'Image analysis', 'Face analysis', 'OCR', 'Thumbnail Image'])
+if choix== 'Image analysis':
+st.file_uploader('load image', type=['png', 'jpg', 'jpeg'])
